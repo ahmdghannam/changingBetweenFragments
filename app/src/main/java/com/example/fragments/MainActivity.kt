41900decffc8c352556abdd946f9e6f82542bcd0
Ahmed Ghannam <iia7mt@gmail.com>
2023-03-30 23:07:45 +0300

    0100 View Pager

--- app/src/main/java/com/example/fragments/MainActivity.kt
+++ app/src/main/java/com/example/fragments/MainActivity.kt
@@ -2,63 +2,71 @@ package com.example.fragments
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import android.widget.Button
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.fragment.app.Fragment
 import com.example.fragments.databinding.ActivityMainBinding
+import com.google.android.material.tabs.TabLayoutMediator
 
 class MainActivity : AppCompatActivity() {
-    private val myFirstFragment = OneFragment()
-    private val mySecondFragment = TwoFragment()
-    private val myThirdFragment = TwoFragment()
+
+    val fragments = listOf(OneFragment(), TwoFragment(), TwoFragment())
+    val tabTitles = listOf("Home", "Friends", "Notifications")
 
     private val binding by lazy {
         ActivityMainBinding.inflate(layoutInflater)
     }
 
-    private var flag = false
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
-        initSubView()
-        addNavigationListener()
+        initViewPager()
+        initTabLayout()
     }
 
-    private fun addNavigationListener() {
-        binding.bottomNavigationView.setOnNavigationItemSelectedListener { item ->
-            when (item.itemId) {
-                R.id.pageHome -> {
-                    replaceFragment(myFirstFragment)
-                    true// is handled as return value
-                }
-                R.id.pageFriends ->{
-                    replaceFragment(mySecondFragment)
-                    true
-                }
-                R.id.pageNotification ->{
-                    replaceFragment(myThirdFragment)
-                    true
-                }
-                else -> false
-            }
-
+    private fun initTabLayout() {
+        val mediator = TabLayoutMediator(
+            binding.tabLayout,
+            binding.viewPager
+        ) { tab, position ->
+            tab.text = tabTitles[position]
         }
+
+        mediator.attach()
     }
 
-    private fun initSubView() {
-        addFragment(myFirstFragment)
+    private fun initViewPager() {
+        val adapter =
+            MyPagerAdapter(this@MainActivity, fragments) // AppCompatActivity is a FragmentActivity
+        binding.viewPager.adapter = adapter
+
     }
 
-    private fun addFragment(fragment: Fragment) {
-        val transaction = supportFragmentManager.beginTransaction()
-        transaction.add(R.id.fragment_container, fragment)
-        transaction.commit()
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.action_bar_menu, menu)
+        return true
     }
 
-    private fun replaceFragment(fragment: Fragment) {
-        val transaction = supportFragmentManager.beginTransaction()
-        transaction.replace(R.id.fragment_container, fragment)
-        transaction.commit()
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.action_delete -> {
+                toast("all the database deleted 😢")
+                true
+            }
+            R.id.action_refresh -> {
+                toast("ok its restored ✌️")
+                true
+            }
+            else -> false
+        }
+
     }
 
+    private fun toast(s: String) {
+        Toast.makeText(this, s, Toast.LENGTH_SHORT).show()
+    }
 
 }
--- app/src/main/java/com/example/fragments/OneFragment.kt
+++ app/src/main/java/com/example/fragments/OneFragment.kt
@@ -1,12 +1,14 @@
 package com.example.fragments
 
 import android.content.Context
+import android.content.DialogInterface
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.fragment.app.Fragment
 import com.example.fragments.databinding.FragmentOneBinding
 
@@ -20,12 +22,36 @@ class OneFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         binding = FragmentOneBinding.inflate(inflater, container, false)
+
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+//        createAlertDialog()
+    }
+
+    private fun createAlertDialog() {
+        val builder = AlertDialog.Builder(requireContext())
+        builder.apply {
+            setTitle("title")
+            setMessage("message")
+            setPositiveButton("yes") { dialog, which ->
+                Toast.makeText(context, "positive clicked", Toast.LENGTH_SHORT).show()
+            }
+            setNegativeButton("no") { dialog, which ->
+                Toast.makeText(context, "negative button clicked", Toast.LENGTH_SHORT).show()
+            }
+            setCancelable(true)
+        }
+        val dialog = builder.create()
+        dialog.show()
     }
 
 }
